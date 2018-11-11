aaad9b32d61c3730af5a98b37e332e2ff885356e
Mallaiya M <mallaiyazap@gmail.com>
2018-11-11 22:32:02 +0530

    Updates

---
+++ precache-manifest.903bccf05356f7363a3d98f3dccc998a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/LTRestaurant/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "582823a750db828e6150",
+    "url": "/LTRestaurant/static/css/main.3b3960b8.chunk.css"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/LTRestaurant/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "963c2ac19f7b3b8e972d",
+    "url": "/LTRestaurant/static/js/1.963c2ac1.chunk.js"
+  },
+  {
+    "revision": "c784928d6e80d3dc15ab",
+    "url": "/LTRestaurant/static/js/runtime~main.c784928d.js"
+  },
+  {
+    "revision": "066b5f4eeaa1a066358494217fe2d3f8",
+    "url": "/LTRestaurant/static/media/food1.066b5f4e.jpg"
+  },
+  {
+    "revision": "66c87204f3d7002f1763bdb7153050bb",
+    "url": "/LTRestaurant/static/media/food2.66c87204.jpg"
+  },
+  {
+    "revision": "b730ecdf00944af1f07276946aa478bb",
+    "url": "/LTRestaurant/static/media/food3.b730ecdf.jpg"
+  },
+  {
+    "revision": "de14dd4dec03c6e50e48517a3fff45ef",
+    "url": "/LTRestaurant/static/media/banner.de14dd4d.jpg"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/LTRestaurant/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/LTRestaurant/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/LTRestaurant/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "582823a750db828e6150",
+    "url": "/LTRestaurant/static/js/main.582823a7.chunk.js"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/LTRestaurant/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/LTRestaurant/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/LTRestaurant/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/LTRestaurant/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/LTRestaurant/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/LTRestaurant/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/LTRestaurant/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/LTRestaurant/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/LTRestaurant/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/LTRestaurant/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "963c2ac19f7b3b8e972d",
+    "url": "/LTRestaurant/static/css/1.3c41e012.chunk.css"
+  },
+  {
+    "revision": "497854cbea90ed92eadb4866e4767616",
+    "url": "/LTRestaurant/index.html"
+  }
+];
